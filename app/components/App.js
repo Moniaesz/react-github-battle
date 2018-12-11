058a28685720853ae13d2e4f4c46dfbfb4c7abd8
Monika <molinskamonika+github@gmail.com>
2018-12-11 08:35:21 +0100

    add Popular.js component with languages section

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 var React = require('react');
+var Popular = require('./Popular');
+
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        Hello Monika World!
-      </div>
-    )
+	return (
+		<div className="container">
+			<Popular />
+		</div>
+	)
   }
 }
 
