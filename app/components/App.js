c49b61c181cf04352a788f09495d02f5e274d99b
Monika <molinskamonika+github@gmail.com>
2019-01-07 10:07:44 +0100

    create nav based on react router

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,10 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 
 
 class App extends React.Component {
@@ -12,7 +15,14 @@ class App extends React.Component {
 		<Router>
 			<div className="container">
 				<Nav />
-				<Route path='/popular' component={Popular} />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/battle' component={Battle} />
+					<Route path='/popular' component={Popular} />
+					<Route render={function() {
+						return <p>Not found</p>
+					}}/>
+				</Switch>
 			</div>
 		</Router>
 		)
